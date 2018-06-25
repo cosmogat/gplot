f160cb88a6160889fb29b61b1694f5d7b5a6a2ec
cosmogat <cosmogatespacial@gmail.com>
2018-06-25 11:31:59 +0200

    bye bye makefile

--- install.sh
+++ install.sh
@@ -4,13 +4,17 @@
 # install.sh
 
 INSTALL_DIR="/usr/local/lib/"
+LIB_NAME="libgplot.so"
 if [ $UID -ne 0 ] ; then  
     echo "Are you root?" >&2 ; exit 1
 fi
 
 cd src
-gcc -fPIC -c gplot.c -o gplot.o
-ld -o libgplot.so gplot.o -shared
+gcc -march=native -O3 -pipe -W -Wall -Wextra -ansi -pedantic -D_GNU_SOURCE -fPIC -c gplot.c -o gplot.o
+ld -o "$LIB_NAME" gplot.o -shared
 rm gplot.o
-mv libgplot.so "$INSTALL_DIR"
+mv "$LIB_NAME" "$INSTALL_DIR"
+cd $INSTALL_DIR
+chown root:staff "$LIB_NAME"
+cd - > /dev/null
 cd ..
